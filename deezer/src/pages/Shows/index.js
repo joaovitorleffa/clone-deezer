3b52108116674016e0b5e4a5b120a838a6a412ef
joaovitorleffa <joaovitorleffa02@gmail.com>
2020-07-28 21:39:03 -0300

    remoção da lib datetimepicker e teste com KeyboardAvoidingView

--- deezer/src/pages/Shows/index.js
+++ deezer/src/pages/Shows/index.js
@@ -1,12 +1,49 @@
 import React from 'react';
-import { View } from 'react-native';
+import { View, KeyboardAvoidingView, TextInput, StyleSheet, Text, Platform, TouchableWithoutFeedback, Button, Keyboard  } from 'react-native';
 
-import Header from '~/assets/components/Header';
-
-const Shows = () => {
+const KeyboardAvoidingComponent = () => {
   return (
-    <Header title="Shows" />
+    <KeyboardAvoidingView
+      behavior={Platform.OS == "ios" ? "padding" : null}
+      keyboardVerticalOffset={Platform.select({ios: 80, android: 500})}
+      style={styles.container}
+    >
+      <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+        <View style={styles.inner}>
+          <Text style={styles.header}>Header</Text>
+          <TextInput placeholder="Username" style={styles.textInput} />
+          <View style={styles.btnContainer}>
+            <Button title="Submit" onPress={() => null} />
+          </View>
+        </View>
+      </TouchableWithoutFeedback>
+    </KeyboardAvoidingView>
   );
-}
+};
 
-export default Shows;
+const styles = StyleSheet.create({
+  container: {
+    flex: 1
+  },
+  inner: {
+    padding: 24,
+    flex: 1,
+    justifyContent: "space-around"
+  },
+  header: {
+    fontSize: 36,
+    marginBottom: 48
+  },
+  textInput: {
+    height: 40,
+    borderColor: "#000000",
+    borderBottomWidth: 1,
+    marginBottom: 36
+  },
+  btnContainer: {
+    backgroundColor: "white",
+    marginTop: 12
+  }
+});
+
+export default KeyboardAvoidingComponent;
